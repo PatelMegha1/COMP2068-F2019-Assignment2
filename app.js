b19a412923fe3402bd76c9d9887863ba65748f9b
Megha Patel <46495166+PatelMegha1@users.noreply.github.com>
2019-11-29 10:52:40 -0500

    update

--- app.js
+++ app.js
@@ -1,33 +1,70 @@
 var createError = require('http-errors');
 var express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 var logger = require('morgan');
-var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/dev').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-mongoose.connect(
-  'mongodb+srv://Tournament:Test123@cluster0-uzvws.mongodb.net/test?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
-var db = mongoose.connection;
-db.on('error', err => console.error(err));
-db.once('open', () => console.log('Connection to mongoose successful'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express session
+app.use(
+  session({
+    secret: '31314424add',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+app.use(function (req, res, next) {
+  res.locals.isLogin = req.isAuthenticated()
+  res.locals.errors = req.flash('errors');
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
